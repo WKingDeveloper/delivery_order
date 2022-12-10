8ab13a4ae9df8592b8eaa25928f4c0182a922d1e
WKingDev <wkingdeveloper@gmail.com>
2022-12-10 17:29:23 +0900

    chore : 서비스 실행 시 Order table, Order data가 자동으로 create, insert 되도록 함.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE `Order`;
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO `Order` (`id`, `address_1`, `address_2`, `paymentStatus`, `platform`, `request`, `price`, `status`, `issued`, `phoneNumber`)
+VALUES
+    (1, '역삼동 709-11 한라아파트','302호', 'DONE', 'SELF', '문 앞에 놔주세요', 32000, 'PAID' , false, '010-8973-8872'),
+    (2, '삼성동 62-1 신라호텔','웨딩홀 2층 302호', 'DONE', 'YOGIYO', '안전 운전해서 와주세요', 30000, 'PACKING' , false, '010-8973-8872'),
+    (3, '삼성동 3-9','603호', 'CARD', 'BAEMIN', '수저,포크 가져다 주세요', 12000, 'ISSUED' , true, "010-8973-8872"),
+    (4, '역삼동 620-12 바로빌라','104동 302호', 'CASH', 'BAEMIN', '문앞에서 연락주세요', 15000, 'ISSUED' , true, '010-8973-8872'),
+    (5, '삼성동 45-1','507호', 'CARD', 'YOGIYO', '', 45000, 'COMPLETED' , true, '010-8973-8872');
+
+COMMIT;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `Order` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `address_1` varchar(1024) NOT NULL,
+    `address_2` varchar(1024) NOT NULL,
+    `paymentStatus` varchar(64) NOT NULL,
+    `platform` varchar(128) NOT NULL,
+    `request` varchar(1024) NOT NULL,
+    `price` decimal(19,2) NOT NULL,
+    `status` varchar(64) NOT NULL,
+    `issued` tinyint(1) NOT NULL,
+    `phoneNumber` varchar(64) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;
